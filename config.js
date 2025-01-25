1139942ff1a2fe704120162df1a153d7f23e9182
briteming <briteming@gmail.com>
2025-01-24 22:24:25 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var _config = {
-    blog_name       : 'CSD',         // 博客名称
-    owner           : 'caisiduo',           // github 用户名
-    repo            : 'i',// github 中对应项目名
-    access_token : '71fd0cd7e63ab8a39c93'+'e983e2c3b1d82e644496',       // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
+    blog_name       : 'blog of brite',         // 博客名称
+    owner           : 'briteming',           // github 用户名
+    repo            : 'ygib',// github 中对应项目名
+    access_token : 'ghp_NhyIYwdpDYx9OtWfa7ui'+'SYnnHmlwpQ3frOrZ',       // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
     per_page        : '4'                    // 默认一页显示几篇文章
 }
